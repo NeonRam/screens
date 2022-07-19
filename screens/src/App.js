bc013fd2292786c9ca962d05dc40120253da46fa
NeonRam <Ram1055@hotmail.com>
2022-07-19 13:06:20 +0000

    first commit

---
+++ screens/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Privacy from './components/privacy';
+import Change from './components/change';
+
+function App() {
+  return (
+    <div className="App">
+      <Privacy/>
+    </div>
+  );
+}
+
+export default App;
